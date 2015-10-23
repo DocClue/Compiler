babbdbc922d982c19b3adfc8767702c84c61564a
fuzzything44 <slimesftw@gmail.com>
2015-10-23 15:23:54 -0600

    Does this commit?

---
+++ Compiler/Compiler/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Compiler
+//
+//  Created by Tanner McCormack on 10/23/15.
+//  Copyright (c) 2015 Tanner McCormack. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
