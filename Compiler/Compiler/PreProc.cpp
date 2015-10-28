ce8719cad63bb02dc6eea10696187e26867ad789
Dr.Clue <twilight71777@gmail.com>
2015-10-28 13:36:58 -0600

    Line Index

--- Compiler/Compiler/PreProc.cpp
+++ Compiler/Compiler/PreProc.cpp
@@ -6,13 +6,22 @@
 
 std::string PreProcess(std::string file)
 {
-    std::ifstream ifs(file, std::ifstream::in);
+    
+    std::ifstream myfileI;
+    std::ofstream myfileO;
+    
     std::string lineInfo;
     std::string command ;
+    std::string outLine ;
+    int lineNum = 0;
     int a = 0;
-    while (ifs) {
+    
+    myfileO.open (file);
+    myfileI.open(file);
+   
+    while (myfileI) {
         
-        getline(ifs,lineInfo);
+        getline(myfileI,lineInfo);
         for (auto i = lineInfo.cbegin(); i!= lineInfo.cend(); i++) {
             if (*i == ' ') {
                 
@@ -20,12 +29,16 @@ std::string PreProcess(std::string file)
             } else if (*i =='#') {
                 command += *i;
                 
+                
             } else {
                 // No preprocessor command on this line.
             }
             a++;
         }
+        lineNum++;
         
+        outLine =  "<" + file + " " + std::to_string(lineNum )+ ">" ;
+        myfileO << outLine;
     }
     
     return "preprocess/" + file;
